d562202c7dc10f2e7445a5d7624269fdd825e6a0
seanonyango <sean.onyango@my.jcu.edu.au>
2024-10-07 22:06:29 +1100

    complete randoms.py

---
+++ prac_03/randoms.py
@@ -0,0 +1,19 @@
+import random
+
+print(random.randint(5, 20))  # line 1
+print(random.randrange(3, 10, 2))  # line 2
+print(random.uniform(2.5, 5.5))  # line 3
+# What did you see on line 1?
+# What was the smallest number you could have seen, what was the largest?
+# I saw 11. The smallest possible number wouyld have been 5 and the largest would be 20
+
+# What did you see on line 2?
+# What was the smallest number you could have seen, what was the largest?
+# Could line 2 have produced a 4?
+# Saw 5. smallest would be 3 and largest would be 9. 4 is impossible
+
+# # What did you see on line 3?
+# # What was the smallest number you could have seen, what was the largest?
+# I saw 3.74...,  The smallest number would be 2.0 and the largest would be just below 5.5
+
+print(random.randint(1,100))
