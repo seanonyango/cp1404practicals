708c4e0a8b46bacd7e4e4bfac60de6effa1efdab
seanonyango <sean.onyango@my.jcu.edu.au>
2024-11-25 20:38:18 +1000

    complete_silver_service taxi

---
+++ prac_09/silver_service_taxi.py
@@ -0,0 +1,15 @@
+from taxi import Taxi
+
+
+class SilverServiceTaxi(Taxi):
+    def __init__(self, name, fuel, fanciness):
+        flagfall = 4.50
+        super().__init__(name, fuel)
+        self.price_per_km *= fanciness
+        self.flagfall = flagfall
+
+    def get_fare(self):
+        return super().get_fare() + self.flagfall
+
+    def __str__(self):
+        return f"{super().__str__()} plus flagfall of {self.flagfall}"
